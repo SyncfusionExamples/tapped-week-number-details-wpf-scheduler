481557fa4658e3a86ca68fa970dace8c3ecd5d45
Jayaleshwari N <jayaleshwarin@syncfusion.com>
2020-11-24 15:09:07 +0530

    WPF-55743 - Sample and screenshot added.

---
+++ SchedulerWPF/SchedulerWPF/Behavior/SchedulerBehavior.cs
@@ -0,0 +1,30 @@
+﻿using System.Windows;
+using Microsoft.Xaml.Behaviors;
+using Syncfusion.UI.Xaml.Scheduler;
+
+namespace WpfScheduler
+{
+    public class SchedulerBehavior : Behavior<SfScheduler>
+    {
+        SfScheduler scheduler;
+        protected override void OnAttached()
+        {
+            base.OnAttached();
+            scheduler = this.AssociatedObject;
+            this.AssociatedObject.WeekNumberTapped += AssociatedObject_WeekNumberTapped;
+        }
+        private void AssociatedObject_WeekNumberTapped(object sender, WeekNumberTappedEventArgs e)
+        {
+            var no = e.WeekNumber.ToString();
+            MessageBox.Show("WeekNumber" + " " + no, "WeekNumberTapped", MessageBoxButton.OK);
+        }
+        protected override void OnDetaching()
+        {
+            base.OnDetaching();
+            this.AssociatedObject.WeekNumberTapped -= AssociatedObject_WeekNumberTapped;
+            this.scheduler = null;
+        }
+    }
+}
+    
+
